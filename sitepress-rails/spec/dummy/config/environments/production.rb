00f70ca5b2234866566789f14236a914dfd814e1
Lucas Nicodemus <shank@shanked.me>
2021-11-12 12:03:56 -0800

    Update production.rb

--- sitepress-rails/spec/dummy/config/environments/production.rb
+++ sitepress-rails/spec/dummy/config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.load_defaults '6.1'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
